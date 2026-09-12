5cee18b794e98015b4f67bfe99cdf829fb2a12d9
prajapatiramswaroop15-creator <prajapatiramswaroop15@gmail.com>
2026-09-12 20:21:43 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Paste ONLY your Supabase Project URL and Publishable key here.
 // Never put a Supabase secret/service_role key in this file.
-window.SUPABASE_URL = 'https://isfzmnumcgzitklfrupr.supabase.co/rest/v1/';
-window.SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_ZhnN2hWJDmKQNGp1PV3UJg_9NzuADrU';
+window.SUPABASE_URL = 'PASTE_YOUR_API_URL_HERE';
+window.SUPABASE_PUBLISHABLE_KEY = 'PASTE_YOUR_PUBLISHABLE_KEY_HERE';
